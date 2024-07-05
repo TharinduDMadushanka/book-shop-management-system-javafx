d9abd455842d5bf80178a31587d288ba85a67ad0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-05 10:16:38 +0530

    complete AdminModel class

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -3,23 +3,27 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DBConnection {
-    private static DBConnection dbConnection;
-    private Connection connection;
+//    private static DBConnection dbConnection;
+//    private Connection connection;
+//
+//    private DBConnection() throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop","root","Thariya920@");
+//    }
+//
+//    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
+//        if (dbConnection == null) {
+//            dbConnection = new DBConnection();
+//        }
+//        return dbConnection;
+//    }
+//    public Connection getConnection() {
+//        return connection;
+//    }
 
-    private DBConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop","root","Thariya920@");
-    }
+    public static ArrayList<User> userTable = new ArrayList();
 
-    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
-        if (dbConnection == null) {
-            dbConnection = new DBConnection();
-        }
-        return dbConnection;
-    }
-    public Connection getConnection() {
-        return connection;
-    }
 }
