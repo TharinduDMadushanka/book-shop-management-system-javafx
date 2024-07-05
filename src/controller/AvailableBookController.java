78a57b89b5596afd7ba7ce96bccff9d81c741623
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-05 14:29:36 +0530

    generate all methods in AvailableBook UI

--- src/controller/AvailableBookController.java
+++ src/controller/AvailableBookController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class AvailableBookController {
+    public AnchorPane bookContext;
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
 }
