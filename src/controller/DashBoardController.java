e1491f24e3b0631a30a74b247071d7897349ae7a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-05 14:02:23 +0530

    genarate all modods in dashBoard UI

--- src/controller/DashBoardController.java
+++ src/controller/DashBoardController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashBoardController {
+    public AnchorPane mainContext;
+    public AnchorPane dashBoardContext;
+
+    public void dashBoardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void availableBooksOnAction(ActionEvent actionEvent) {
+    }
+
+    public void purchaseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signOutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
 }
